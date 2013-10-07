6fcc9246002a5afed83b1f777d20ef1d2442752d
irgmedeiros <irgmedeiros@gmail.com>
2013-10-07 16:47:19 -0300

    Remove unnecessary code

--- folhainvest/main.py
+++ folhainvest/main.py
@@ -3,10 +3,9 @@
 
 from twisted.internet import reactor
 from scrapy.crawler import Crawler
-from scrapy.settings import Settings
 from scrapy import log, signals
 from folhainvest.spiders.Finvest_spider import Finvest_spider
-from scrapy.utils.project import inside_project, get_project_settings
+from scrapy.utils.project import get_project_settings
 
 
 def stop_reactor(spider, reason):
@@ -15,9 +14,7 @@ def stop_reactor(spider, reason):
 
 
 spider = Finvest_spider(domain='folhainvest.folha.com.br')
-
 settings = get_project_settings()
-#crawler = Crawler(Settings())
 crawler = Crawler(settings)
 crawler.signals.connect(stop_reactor, signal=signals.spider_closed)
 crawler.configure()
@@ -28,11 +25,8 @@ log.msg('Running reactor...')
 reactor.run()  # the script will block here until the spider is closed
 log.msg('Reactor stopped.')
 
-l = log.log.discardLogs()
-print s
 
-
-from scrapy import cmdline
-cmdline.execute("scrapy crawl finvest_spider".split())
+#from scrapy import cmdline
+#cmdline.execute("scrapy crawl finvest_spider".split())
 
 
