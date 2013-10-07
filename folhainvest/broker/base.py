753ccb7e18d397308ba4ed487b7070da0911040f
irgmedeiros <irgmedeiros@gmail.com>
2013-10-07 16:04:54 -0300

    Create broker module

---
+++ folhainvest/broker/base.py
@@ -0,0 +1,44 @@
+# coding: utf-8
+import mechanize
+import cookielib
+
+class BaseBroker(object):
+    portifolio = None
+    cookies = dict()
+
+    def authenticate(self, user, password):
+        """
+        Returns
+        --------
+        Return
+        """
+        raise NotImplementedError
+
+    def sell(self, symbol, shares, price):
+        """
+        Returns
+        --------
+        Return
+        """
+        raise NotImplementedError
+
+
+    def buy(self, symbol, shares, price):
+        """
+        Returns
+        --------
+        Return
+        """
+        raise NotImplementedError
+
+
+def login_check(method):
+    """Checker decorator"""
+
+    def wrapper(self, *args, **kwargs):
+        if self.browser:
+            return method(self, *args, **kwargs)
+        else:
+            raise Exception("Not logged!")
+
+    return wrapper
